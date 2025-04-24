0f034c658c59d719a584ebb4e61f4b40923f5c9c
nohapf13 <for17004@byui.edu>
2025-04-24 09:58:00 -0600

    Created name assignment in sandbox

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your full name is {lastName}, {firstName} {lastName}.");
     }
 }
