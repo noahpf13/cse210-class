0fed531359d2daecfef19e24047c8fe6e3e14830
nohapf13 <for17004@byui.edu>
2025-04-24 16:27:36 -0600

    Prep1 and Prep2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-        Console.Write("Enter your first name: ");
+        Console.Write("What is your first name: ");
         string firstName = Console.ReadLine();
-        Console.Write("Enter your last name: ");
+        Console.Write("What is your last name: ");
         string lastName = Console.ReadLine();
-        Console.WriteLine($"Your full name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get user input and change to int
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int x = int.Parse(userInput);
+
+        // Letter grade variable
+        string letter = "";
+
+        // Determine letter grade
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print letter grade
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        // Pass or Fail statement
+        if (x >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!!!");
+        }
     }
 }
