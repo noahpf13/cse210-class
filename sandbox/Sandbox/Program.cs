31c9afbea30572a75e80f605b11b352a0b8d4624
nohapf13 <for17004@byui.edu>
2025-04-29 09:39:08 -0600

    Added for loop to Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Sandbox World!");
+        int x = 10;
+        for(int i = 0; i < x; i++) // initial statement; limit or conditional statement; increment structure of for loop
+        {
+            Console.WriteLine("Hello Sandbox World!");
+        }
+        
     }
 }
