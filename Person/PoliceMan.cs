d1177caf32e9d7ff4b230c6bef1af1e13721b9fd
nohapf13 <for17004@byui.edu>
2025-06-08 14:46:35 -0600

    inheritance practice

--- Person/PoliceMan.cs
+++ Person/PoliceMan.cs
@@ -2,9 +2,13 @@ class PoliceMan : Person
 {
     private string _weapons;
 
-    public PoliceMan(string firstName, string lastName, int age) 
+    public PoliceMan(string firstName, string lastName, int age, string weapons)
     : base(firstName, lastName, age)
     {
-        
+        _weapons = weapons;
+    }
+    public string GetPoliceManInformation()
+    {
+        return $"Weapons: {_weapons} :: {GetPersonInformation()}";
     }
 }
--- Person/Program.cs
+++ Person/Program.cs
@@ -5,7 +5,11 @@
         Person myperson = new Person("Bobby", "Bob", 53);
         Console.WriteLine(myperson.GetPersonInformation());
 
-        PoliceMan myPoliceMan = new PoliceMan("Cooper", "Silver", 35);
+        PoliceMan myPoliceMan = new PoliceMan("Cooper", "Silver", 35, "Club");
         Console.WriteLine(myPoliceMan.GetPersonInformation());
+        Console.WriteLine(myPoliceMan.GetPoliceManInformation());
+
+        Doctor myDoctor = new Doctor("Bob", "Payne", 65, "Stethoscope");
+        Console.WriteLine(myDoctor.GetDoctorInformation());
     }
 }
