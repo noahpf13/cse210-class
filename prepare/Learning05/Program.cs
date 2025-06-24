1872ad9dc115b5133236e34ca378a1e7fe1bb337
nohapf13 <for17004@byui.edu>
2025-06-23 18:28:42 -0600

    wk 9 shapes stuff

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create shapes
+        Shape square = new Square("Red", 4);
+        Shape rectangle = new Rectangle("Blue", 5, 6);
+        Shape circle = new Circle("Green", 3);
+
+        // Store in a list
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        // Display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
+}
+
 }
